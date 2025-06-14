83a5e767de99c8a6bed306e1251cc30c2b58814d
Chris91ss <trillcriss@gmail.com>
2025-06-14 14:09:09 +0300

    .

---
+++ Second year/Second Semester/DBM/DatabaseManagementApp Practical/DatabaseManagementApp/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace DatabaseManagementApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCities = new System.Windows.Forms.DataGridView();
+            this.dgvFavouriteBakeries = new System.Windows.Forms.DataGridView();
+            this.btnAddEmployee = new System.Windows.Forms.Button();
+            this.btnDeleteEmployee = new System.Windows.Forms.Button();
+            this.btnUpdateEmployees = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCities)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFavouriteBakeries)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCities
+            // 
+            this.dgvCities.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCities.Location = new System.Drawing.Point(1, -1);
+            this.dgvCities.Name = "dgvCities";
+            this.dgvCities.RowHeadersWidth = 51;
+            this.dgvCities.RowTemplate.Height = 24;
+            this.dgvCities.Size = new System.Drawing.Size(616, 255);
+            this.dgvCities.TabIndex = 0;
+            // 
+            // dgvFavouriteBakeries
+            // 
+            this.dgvFavouriteBakeries.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFavouriteBakeries.Location = new System.Drawing.Point(1, 260);
+            this.dgvFavouriteBakeries.Name = "dgvFavouriteBakeries";
+            this.dgvFavouriteBakeries.RowHeadersWidth = 51;
+            this.dgvFavouriteBakeries.RowTemplate.Height = 24;
+            this.dgvFavouriteBakeries.Size = new System.Drawing.Size(616, 256);
+            this.dgvFavouriteBakeries.TabIndex = 1;
+            // 
+            // btnAddEmployee
+            // 
+            this.btnAddEmployee.Location = new System.Drawing.Point(623, 281);
+            this.btnAddEmployee.Name = "btnAddEmployee";
+            this.btnAddEmployee.Size = new System.Drawing.Size(108, 23);
+            this.btnAddEmployee.TabIndex = 2;
+            this.btnAddEmployee.Text = "Add";
+            this.btnAddEmployee.UseVisualStyleBackColor = true;
+            this.btnAddEmployee.Click += new System.EventHandler(this.btnAddEmployee_Click);
+            // 
+            // btnDeleteEmployee
+            // 
+            this.btnDeleteEmployee.Location = new System.Drawing.Point(623, 339);
+            this.btnDeleteEmployee.Name = "btnDeleteEmployee";
+            this.btnDeleteEmployee.Size = new System.Drawing.Size(108, 23);
+            this.btnDeleteEmployee.TabIndex = 3;
+            this.btnDeleteEmployee.Text = "Delete Employee";
+            this.btnDeleteEmployee.UseVisualStyleBackColor = true;
+            this.btnDeleteEmployee.Click += new System.EventHandler(this.btnDeleteEmployee_Click);
+            // 
+            // btnUpdateEmployees
+            // 
+            this.btnUpdateEmployees.Location = new System.Drawing.Point(623, 396);
+            this.btnUpdateEmployees.Name = "btnUpdateEmployees";
+            this.btnUpdateEmployees.Size = new System.Drawing.Size(108, 23);
+            this.btnUpdateEmployees.TabIndex = 4;
+            this.btnUpdateEmployees.Text = "Update Employees";
+            this.btnUpdateEmployees.UseVisualStyleBackColor = true;
+            this.btnUpdateEmployees.Click += new System.EventHandler(this.btnUpdateEmployees_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 528);
+            this.Controls.Add(this.btnUpdateEmployees);
+            this.Controls.Add(this.btnDeleteEmployee);
+            this.Controls.Add(this.btnAddEmployee);
+            this.Controls.Add(this.dgvFavouriteBakeries);
+            this.Controls.Add(this.dgvCities);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCities)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFavouriteBakeries)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCities;
+        private System.Windows.Forms.DataGridView dgvFavouriteBakeries;
+        private System.Windows.Forms.Button btnAddEmployee;
+        private System.Windows.Forms.Button btnDeleteEmployee;
+        private System.Windows.Forms.Button btnUpdateEmployees;
+    }
+}
+
---
+++ Second year/Second Semester/DBM/DatabaseManagementApp Practical/DatabaseManagementApp/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace DatabaseManagementApp
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString =
+            ConfigurationManager.ConnectionStrings["MyDbConnection"].ConnectionString;
+
+        private SqlConnection conn;
+        private SqlDataAdapter daCities, daBakeries;
+        private SqlCommandBuilder cbBakeries;
+        private DataSet ds;
+
+        private BindingSource bsCities = new BindingSource();
+        private BindingSource bsBakeries = new BindingSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+            LoadData();
+            SetupDataBinding();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                ds = new DataSet();
+
+                // 1. Load Cities (parent)
+                daCities = new SqlDataAdapter("SELECT * FROM Cities", conn);
+                daCities.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+                daCities.Fill(ds, "Cities");
+
+                // 2. Load FavouriteBakeries (child)
+                daBakeries = new SqlDataAdapter("SELECT * FROM FavouriteBakeries", conn);
+                daBakeries.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+
+                // Custom InsertCommand
+                SqlCommand insertCmd = new SqlCommand(@"
+            INSERT INTO FavouriteBakeries (Name, StartupYear, NumberOfStars, CityID)
+            VALUES (@Name, @StartupYear, @NumberOfStars, @CityID);
+            SELECT CAST(SCOPE_IDENTITY() AS int);", conn);
+
+                insertCmd.Parameters.Add("@Name", SqlDbType.NVarChar, 100, "Name");
+                insertCmd.Parameters.Add("@StartupYear", SqlDbType.Int, 0, "StartupYear");
+                insertCmd.Parameters.Add("@NumberOfStars", SqlDbType.Int, 0, "NumberOfStars");
+                insertCmd.Parameters.Add("@CityID", SqlDbType.Int, 0, "CityID");
+                insertCmd.UpdatedRowSource = UpdateRowSource.FirstReturnedRecord;
+                daBakeries.InsertCommand = insertCmd;
+
+                cbBakeries = new SqlCommandBuilder(daBakeries);
+                daBakeries.UpdateCommand = cbBakeries.GetUpdateCommand();
+                daBakeries.DeleteCommand = cbBakeries.GetDeleteCommand();
+
+                daBakeries.Fill(ds, "FavouriteBakeries");
+
+                ds.Tables["FavouriteBakeries"].Columns["BakeryID"].AllowDBNull = true;
+
+                DataRelation rel = new DataRelation("City_Bakeries",
+                    ds.Tables["Cities"].Columns["CityID"],
+                    ds.Tables["FavouriteBakeries"].Columns["CityID"]);
+                ds.Relations.Add(rel);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading data: " + ex.Message);
+            }
+        }
+
+
+        private void SetupDataBinding()
+        {
+            try
+            {
+                bsCities.DataSource = ds;
+                bsCities.DataMember = "Cities";
+
+                bsBakeries.DataSource = bsCities;
+                bsBakeries.DataMember = "City_Bakeries";
+
+                dgvCities.DataSource = bsCities;
+                dgvFavouriteBakeries.DataSource = bsBakeries;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error setting up data binding: " + ex.Message);
+            }
+        }
+
+        private void btnAddEmployee_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Ensure a city is selected
+                if (bsCities.Current == null)
+                {
+                    MessageBox.Show("Please select a city first.");
+                    return;
+                }
+
+                DataRowView newRowView = (DataRowView)bsBakeries.AddNew();
+                newRowView["Name"] = "New Bakery";
+                newRowView["StartupYear"] = DateTime.Now.Year;
+                newRowView["NumberOfStars"] = 0;
+                newRowView["CityID"] = ((DataRowView)bsCities.Current)["CityID"]; 
+
+                bsBakeries.EndEdit();
+                MessageBox.Show("New bakery added in-memory. Click 'Update' to save.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error adding new bakery: " + ex.Message);
+            }
+        }
+
+
+        private void btnDeleteEmployee_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (bsBakeries.Current != null)
+                {
+                    bsBakeries.RemoveCurrent();
+                    MessageBox.Show("Bakery removed in-memory. Click 'Update' to finalize.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error deleting bakery: " + ex.Message);
+            }
+        }
+
+        private void btnUpdateEmployees_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bsBakeries.EndEdit();
+                daBakeries.Update(ds, "FavouriteBakeries");
+                MessageBox.Show("Bakeries updated successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error updating bakeries: " + ex.Message);
+            }
+        }
+    }
+}
